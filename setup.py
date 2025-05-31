35d37dc83e2eb0e619fdb00be9b3e0e98a29402c
charlieikosi <73019564+charlieikosi@users.noreply.github.com>
2025-06-01 11:15:23 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,24 @@ setup(
     name='RemoteSensingTools',  # This will be the name used in pip install
     version='0.1',
     packages=find_packages(),
-    install_requires=[],  # Add dependencies here
+    install_requires=[
+        "planetary-computer",
+        "pystac-client",
+        "rioxarray",
+        "geopandas",
+        "matplotlib",
+        "xarray",
+        "shapely",
+        "numpy",
+        "dask",
+        "rasterio",
+        "ipython",
+        "rich",
+        "odc-stac",
+        "pyproj",
+        "folium",
+        "stackstac"
+    ],  # Add dependencies here
     author='Charlie A Ikosi',
     author_email='charlieikosi@gmail.com',
     description='Functions for working with imagery on Planetary Computer',
